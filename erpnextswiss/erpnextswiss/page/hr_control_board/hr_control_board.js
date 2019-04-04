e58335b2d88f277ae1dbe55acef8307da4c28a00
Joelios <joel.mesmer@libracore.com>
2019-04-05 00:21:33 +0200

    hc control board

--- erpnextswiss/erpnextswiss/page/hr_control_board/hr_control_board.js
+++ erpnextswiss/erpnextswiss/page/hr_control_board/hr_control_board.js
@@ -26,8 +26,15 @@ frappe.hr_control_board = {
 			args: {},
 			callback: function(r) {
 				if (r.message) {
-					var placeholder = $('#employee_content_placeholder');
-					$(frappe.render_template('employee_content', {employees: r.message.employees, earnings: r.message.earnings, deductions: r.message.deductions})).appendTo(placeholder);
+					var placeholder = $('#employee_content_placeholder').empty();
+					$(frappe.render_template('employee_content', {
+							employees: r.message.employees,
+							earnings: r.message.earnings,
+							deductions: r.message.deductions,
+							already_paid: r.message.already_paid,
+							cap_alv_1: r.message.cap_alv_1,
+							cap_alv_2: r.message.cap_alv_2,
+							cap_uvg: r.message.cap_uvg})).appendTo(placeholder);
 				} 
 			}
 		});
@@ -38,4 +45,32 @@ function open_employee(btn) {
 	var url = '/desk#Form/Employee/' + $(btn).attr("data-employee");
 	var win = window.open(url, '_blank');
 	win.focus();
+}
+
+function change_sal_typ(btn) {
+	$(btn).parent().addClass("active");
+	$(btn).parent().siblings().removeClass("active");
+	if ($(btn).parent().attr("id") == 'm_sal') {
+		frappe.hr_control_board.show_employee_content();
+	} else {
+		frappe.call({
+			method: 'erpnextswiss.erpnextswiss.page.hr_control_board.hr_control_board.get_employee_content',
+			args: {
+				'timesheet': 1
+			},
+			callback: function(r) {
+				if (r.message) {
+					var placeholder = $('#employee_content_placeholder').empty();
+					$(frappe.render_template('employee_content', {
+							employees: r.message.employees,
+							earnings: r.message.earnings,
+							deductions: r.message.deductions,
+							already_paid: r.message.already_paid,
+							cap_alv_1: r.message.cap_alv_1,
+							cap_alv_2: r.message.cap_alv_2,
+							cap_uvg: r.message.cap_uvg})).appendTo(placeholder);
+				} 
+			}
+		});
+	}
 }
--- erpnextswiss/erpnextswiss/page/hr_control_board/hr_control_board.py
+++ erpnextswiss/erpnextswiss/page/hr_control_board/hr_control_board.py
@@ -11,12 +11,17 @@
 import frappe
 
 @frappe.whitelist()
-def get_employee_content():
+def get_employee_content(year=None, timesheet=None):
 	data = {}
+	ts_filter = ''
+	if timesheet:
+		ts_filter = ' WHERE `salary_based_on_timesheets` = 1'
+	else:
+		ts_filter = ' WHERE `salary_based_on_timesheets` = 0'
 	data['employees'] = frappe.db.sql("""SELECT
 								`name`,
 								`employee_name`
-							FROM `tabEmployee`""", as_dict=True)
+							FROM `tabEmployee`{ts_filter}""".format(ts_filter=ts_filter), as_dict=True)
 							
 	data['earnings'] = frappe.db.sql("""SELECT
 											`salary_component`,
@@ -42,4 +47,17 @@ def get_employee_content():
 										WHERE `parentfield` = 'deductions'
 										AND `parenttype` = 'Employee'""", as_dict=True)
 										
+	if year:
+		filter = """ AND YEAR(`posting_date`) = '{year}'""".format(year=year)
+	else:
+		filter = ' AND YEAR(`posting_date`) = YEAR(CURDATE())'
+	data['already_paid'] = frappe.db.sql("""SELECT `employee`, SUM(`gross_pay`) AS 'gross_pay' FROM `tabSalary Slip` WHERE `status` = 'Submitted'{filter} GROUP BY `employee`""".format(filter=filter), as_dict=True)
+	
+	data['cap_alv_1'] = default_settings().cap_alv_1
+	data['cap_alv_2'] = default_settings().cap_alv_2
+	data['cap_uvg'] = default_settings().cap_uvg
 	return data
+	
+def default_settings():
+	return frappe.get_single('HR Default Settings')
+	
